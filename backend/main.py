aacfcf624a1d2bd59aab8027d9531288d0514ff8
Ilona Kovaleva <iloncka.ds@gmail.com>
2025-05-12 19:36:01 +0000

    May 12, 2025, 10:36 PM

--- backend/main.py
+++ backend/main.py
@@ -90,8 +90,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from .config import settings
-from .routers import filters, species, geo
+from backend.config import settings
+from backend.routers import filters, species, geo
 
 app = FastAPI(title=settings.APP_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json")
 
