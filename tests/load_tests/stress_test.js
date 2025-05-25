8102bf6f4355763e8ad539e1377ca1857eef3665
Ilona Kovaleva <iloncka.ds@gmail.com>
2025-05-25 23:02:19 +0300

    ✨ Adds load test scripts
    Adds K6 load test scripts for different testing scenarios: load, simple, soak, spike, and stress tests.
    
    These scripts define test configurations, performance thresholds, and reporting functionalities.

---
+++ tests/load_tests/stress_test.js
@@ -0,0 +1,51 @@
+// You typically want to stress test an API or website to determine:
+
+//     - How your system will behave under extreme conditions.
+//     - What the maximum capacity of your system is in terms of users or throughput.
+//     - The breaking point of your system and its failure mode.
+//     - If your system will recover without manual intervention after the stress test is over.
+
+import http from 'k6/http';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { check, sleep } from 'k6';
+
+
+export const options = {
+
+    insecureSkipTLSVerify: true,
+    noConnectionReuse: false,
+
+    stages: [
+        { duration: '2m', target: 100 }, // below normal load
+        { duration: '5m', target: 100 },
+        { duration: '2m', target: 200 }, // normal load
+        { duration: '5m', target: 200 },
+        { duration: '2m', target: 300 }, // around the breaking point
+        { duration: '5m', target: 300 },
+        { duration: '2m', target: 400 }, // beyond the breaking point
+        { duration: '5m', target: 400 },
+        { duration: '10m', target: 0 }, // scale down. Recovery stage.
+        ],
+
+};
+
+// for (let id = 1; id <= 100; id++) {
+//     http.get(`http://localhost:8001/item/${id}?k=3`, {
+//       tags: { name: 'PostsItemURL' },
+//     });
+//   }
+
+export default function () {
+
+    const res = http.get('http://127.0.0.1:8000/api/filter_options');
+
+    check(res, { 'status was 200': (r) => r.status == 200 });
+
+    sleep(1);
+
+}
+export function handleSummary(data) {
+        return {
+        "summary_stress_test.html": htmlReport(data),
+        };
+    }
